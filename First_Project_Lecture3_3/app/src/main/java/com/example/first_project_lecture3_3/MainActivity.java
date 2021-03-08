d89d51ca466d4bca224d96c7b96faa627e5c3575
Abdul Rehman <mcsf19m046@pucit.edu.pk>
2021-03-08 19:07:47 +0500

    third screen

---
+++ First_Project_Lecture3_3/app/src/main/java/com/example/first_project_lecture3_3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.first_project_lecture3_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
