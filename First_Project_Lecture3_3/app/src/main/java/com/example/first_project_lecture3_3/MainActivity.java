1ac1535ede326479c12b8b7ca7ec242701fb2e09
Abdul Rehman <mcsf19m046@pucit.edu.pk>
2021-04-07 07:12:41 +0500

    'updated'

--- First_Project_Lecture3_3/app/src/main/java/com/example/first_project_lecture3_3/MainActivity.java
+++ First_Project_Lecture3_3/app/src/main/java/com/example/first_project_lecture3_3/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.first_project_lecture3_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    int[] images = {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i
+            , R.drawable.j, R.drawable.k, R.drawable.l, R.drawable.m, R.drawable.n, R.drawable.o, R.drawable.p, R.drawable.q, R.drawable.r
+            , R.drawable.s, R.drawable.t, R.drawable.u, R.drawable.v, R.drawable.w, R.drawable.x, R.drawable.y, R.drawable.z};
+    int startIndex = 0, endIndex = images.length;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.c);
+
+        imageView = findViewById(R.id.imageView);
+        mp = MediaPlayer.create(this, R.raw.sound);
+
+        imageView.setImageResource(images[0]);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (startIndex < endIndex) {
+                    imageView.setImageResource(images[startIndex]);
+                    startIndex++;
+                }
+                mp.start();
+            }
+        });
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+
     }
 }
